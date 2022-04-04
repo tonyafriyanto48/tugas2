a44c53a44caf59913452909c50a8185bd76f32b6
DESKTOP-J7N1A03\Tony Ajeke <tony.afriyanto48@gmail.com>
2022-04-04 10:39:11 -0700

    no message

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tugas2/screen/profil_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileDetails(),
+    );
+  }
+}
